a5340eb29af1fa35a7234cfc47527e69051a943b
sam@smjdesign.co.uk <sam@smjdesign.co.uk>
2020-11-20 15:40:46 +0000

    Initial commit

---
+++ Notes.md
@@ -0,0 +1,5 @@
+https://stackoverflow.com/questions/2058146/what-real-world-uses-of-the-stack-object-net-have-you-used
+
+Use stack as undo last action?
+
+https://gist.github.com/HunterBoadwine/9c32d4aa26e49c7c79342070076807c3
---
+++ Queues/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace Queues
+{
+    class Program
+    {
+        private static Queue<Item> queue = new Queue<Item>();
+
+        static void Main(string[] args)
+        {
+            var size = 10;
+            for (int i = 0; i < size; i++)
+            {
+                queue.Enqueue(new Item
+                {
+                    Message = $"Message added at {DateTime.Now}"
+                });
+                Thread.Sleep(500);
+            }
+
+            while (queue.TryDequeue(out var item))
+            {
+                Console.WriteLine($"{item.Message}");
+                Console.WriteLine($"removing item, queue length: {queue.Count}");
+            }
+        }
+    }
+
+    class Item{
+        public string Message { get; set; }
+    }
+}
